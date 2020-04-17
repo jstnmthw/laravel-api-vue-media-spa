a1a0765839cb15d7dbcf2b4893c98aceefd4e912
Justin <web@jstn.ly>
2020-04-17 17:00:56 +0700

    Added Artisan console command and removed Laravel/Telescope

--- app/Console/Commands/setVideoCategories.php
+++ app/Console/Commands/setVideoCategories.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use DB;
 use Illuminate\Console\Command;
+use App\Categories;
+use App\VideoData;
+use App\VideoCategories;
 
 class setVideoCategories extends Command
 {
@@ -11,14 +15,14 @@ class setVideoCategories extends Command
      *
      * @var string
      */
-    protected $signature = 'db:video_categories';
+    protected $signature = 'video:categorize';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Chunk through a large denormalized dataset and parse/insert categories to a seperate table.';
+    protected $description = 'Chunk through a large denormalized video csv dataset and parse/insert categories to a seperate table.';
 
     /**
      * Create a new command instance.
@@ -37,6 +41,43 @@ class setVideoCategories extends Command
      */
     public function handle()
     {
-        //
+
+        if ($this->confirm('Are you sure you want to continue?')) {
+
+            $categories = Categories::all()->toArray();
+
+            $total = DB::select('SELECT count(views) as count FROM video_data');
+
+            $progressBar = $this->output->createProgressBar($total[0]->count);
+            $progressBar->start();
+            // update every 100 iterations
+            $progressBar->setRedrawFrequency(100);
+
+            VideoData::orderBy('id')->chunk(500, function ($data) use ($progressBar, $categories) {
+
+                foreach ($data as $key => $value) {
+            
+                    $cats = explode(';', $value['categories']);
+
+                    foreach ($cats as $cat_key => $cat_value) {
+
+                        if ($cat = array_search($cat_value, array_column($categories, 'name'))){
+
+                            VideoCategories::insert(['cat_id' => $cat + 1, 'video_data_id' => $value['id']]);
+
+                        }
+
+                    }
+                    
+                    $progressBar->advance();
+
+                }
+
+            });
+
+            $progressBar->finish();
+
+        }
     }
+
 }
--- app/VideoCategories.php
+++ app/VideoCategories.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class VideoCategories extends Model
 {
     
+    /**
+     * Video's have many categories
+     */
+    public function category()
+    {
+        return $this->hasMany('App\Categories');
+    }
 }
