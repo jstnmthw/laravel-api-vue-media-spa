b9830fcac9ca77c8dc80f215bad1bbdfc0b38eab
Justin <web@jstn.ly>
2020-04-18 00:20:43 +0700

    No longer using escape likes. Hooray!

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -153,24 +153,7 @@ class VideoController extends Controller
     {
         //
     }
-
-    /**
-     * Escape special characters for a LIKE query.
-     *
-     * @param string $value
-     * @param string $char
-     *
-     * @return string
-     */
-    private function escape_like(string $value, string $char = '\\'): string
-    {
-        return str_replace(
-            [$char, '%', '_'],
-            [$char.$char, $char.'%', $char.'_'],
-            $value
-        );
-    }
-
+    
     /**
      * Match Category param with database and return category ID.
      * 
