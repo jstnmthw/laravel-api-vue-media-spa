b6435a442a66f67088b0fc3ca73bf1a88d496254
Justin <web@jstn.ly>
2020-05-29 15:37:15 +0700

    Added: Laravel's Scout package with Algolia.

--- app/Video.php
+++ app/Video.php
@@ -4,9 +4,34 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Laravel\Scout\Searchable;
 
 class Video extends Model
 {
+    use Searchable;
+
+    /**
+     * Get the index name for the model.
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'dev_VIDEOS';
+    }
+    
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        return $array;
+    }
+
     /**
      * Get the categories for the video.
      */
