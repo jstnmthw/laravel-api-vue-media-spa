d212ab3840d2daf9961f9640693b78840a736af4
Justin <web@jstn.ly>
2020-11-14 18:00:51 +0700

    Updated MediaSeeder to create a default set of categories.

--- database/factories/MediaFactory.php
+++ database/factories/MediaFactory.php
@@ -23,12 +23,25 @@ class MediaFactory extends Factory
     public function definition()
     {
         $this->faker->addProvider(new Youtube($this->faker));
+
+        $categories = [
+            'General',
+            'News',
+            'Weather',
+            'Politics',
+            'Gaming',
+            'Gossip',
+            'How-to',
+            'Events',
+            'Vlog',
+        ];
+
         return [
             'embed' => $this->faker->youtubeShortUri(),
             'thumbnail' => 'https://picsum.photos/205/155/?image='.$this->faker->randomDigit,
             'album' => 'https://picsum.photos/205/155/?image='.$this->faker->randomDigit.';'.'https://picsum.photos/205/155/?image='.$this->faker->randomDigit.';'.'https://picsum.photos/205/155/?image='.$this->faker->randomDigit,
             'title' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
-            'categories' => $this->faker->word .";". $this->faker->word .";". $this->faker->word,
+            'categories' => $categories[$this->faker->numberBetween(0,8)].";".$categories[$this->faker->numberBetween(0,8)].";".$categories[$this->faker->numberBetween(0,8)],
             'author' => $this->faker->name,
             'duration' => $this->faker->numberBetween($min = 30, $max = 1000),
             'views' => $this->faker->numberBetween($min = 500, $max = 1000000),
