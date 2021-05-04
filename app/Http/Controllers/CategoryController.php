3649ed8ae90ff44abc2082afd9b9b36910fe3d9a
Justin <web@jstn.ly>
2021-05-04 21:16:51 +0700

    update: Remove unused classes

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Str;
 use App\Category;
 
 class CategoryController extends Controller
@@ -17,7 +14,6 @@ class CategoryController extends Controller
      */
     public function index(): JsonResponse
     {
-        app('debugbar')->disable();
         $data = Category::query()
             ->select(['name'])
             ->get()
