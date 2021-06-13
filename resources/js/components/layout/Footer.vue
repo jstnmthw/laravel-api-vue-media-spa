da6fe13aac0f87d983539dd17a47ef8d762114c0
Justin <web@jstn.ly>
2021-06-13 12:48:46 +0700

    fixed: Header hierarchy

--- resources/js/components/layout/Footer.vue
+++ resources/js/components/layout/Footer.vue
@@ -2,13 +2,13 @@
   <footer class="footer container">
     <div class="col-10 mx-auto d-flex">
       <ul class="list-unstyled mr-5">
-        <li><h5>Information</h5></li>
+        <li>Information</li>
         <li><a href="#">Terms &amp; Conditions</a></li>
         <li><a href="#">Privacy</a></li>
         <li><a href="#">DMCA</a></li>
       </ul>
       <ul class="list-unstyled">
-        <li><h5>Help</h5></li>
+        <li>Help</li>
         <li><a href="#">FAQ</a></li>
         <li><a href="#">Contact Us</a></li>
       </ul>
